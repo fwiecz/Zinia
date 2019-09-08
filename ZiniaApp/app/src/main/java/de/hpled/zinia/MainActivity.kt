60d526430fb0f140fa642b63ef081fe0e3e4abe2
Fabian Wieczorek <6wieczor@informatik.uni-hamburg.de>
2019-09-08 21:10:26 +0200

    Addded PropertiesViewModel; Added Nightmode option (more important than anything!); Changed Activity Label;

--- ZiniaApp/app/src/main/java/de/hpled/zinia/MainActivity.kt
+++ ZiniaApp/app/src/main/java/de/hpled/zinia/MainActivity.kt
@@ -2,11 +2,42 @@ package de.hpled.zinia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.lifecycle.ViewModelProviders
 
 class MainActivity : AppCompatActivity() {
+
+    private val appProperties : AppPropertiesViewModel by lazy {
+        ViewModelProviders.of(this).get(AppPropertiesViewModel::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        appProperties.switchNightmode(appProperties.getNightmode())
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_option_menu, menu)
+        return menu != null
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        menu?.findItem(R.id.main_options_menu_nightmode)?.isChecked = appProperties.getNightmode()
+        return menu != null
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if(item != null) {
+            when(item.itemId) {
+                R.id.main_options_menu_nightmode -> {
+                    item.isChecked = !item.isChecked
+                    appProperties.setNightmode(item.isChecked)
+                }
+            }
+        }
+        return true
     }
 }
