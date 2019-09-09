c5b948a9534db976c79040dc2ccf90fed63281e1
Fabian Wieczorek <6wieczor@informatik.uni-hamburg.de>
2019-09-09 22:38:54 +0200

    Added StatusIndicatorView to indicate device availability

---
+++ ZiniaApp/app/src/main/java/de/hpled/zinia/StatusIndicatorView.kt
@@ -0,0 +1,53 @@
+package de.hpled.zinia
+
+import android.content.Context
+import android.os.Handler
+import android.util.AttributeSet
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.ProgressBar
+
+/**
+ * Represents a status of something with a little circle. Four states are available:
+ * unknown, loading, success and error.
+ */
+class StatusIndicatorView(c: Context, attr: AttributeSet?) : FrameLayout(c, attr) {
+
+    enum class State {
+        UNKNOWN,
+        LOADING,
+        SUCCESS,
+        ERROR
+    }
+
+    private val unknownIcon by lazy { findViewById<ImageView>(R.id.statusIndicatorUnknown)}
+    private val progressBar by lazy { findViewById<ProgressBar>(R.id.statusIndicatorProgress)}
+    private val successIcon by lazy { findViewById<ImageView>(R.id.statusIndicatorSuccess)}
+    private val errorIcon by lazy { findViewById<ImageView>(R.id.statusIndicatorError)}
+    private val icons by lazy { listOf(progressBar, unknownIcon, successIcon, errorIcon) }
+
+    /**
+     * The current status.
+     */
+    var status = State.UNKNOWN
+        set(value) {
+            field = value
+            showRespectiveIcon(value)
+        }
+
+    init {
+        View.inflate(context, R.layout.view_status_indicator, this)
+        showRespectiveIcon(status)
+    }
+
+    private fun showRespectiveIcon(state: State) {
+        icons.forEach { it.visibility = View.INVISIBLE}
+        when(state) {
+            State.UNKNOWN -> unknownIcon.visibility = View.VISIBLE
+            State.LOADING -> progressBar.visibility = View.VISIBLE
+            State.SUCCESS -> successIcon.visibility = View.VISIBLE
+            State.ERROR -> errorIcon.visibility = View.VISIBLE
+        }
+    }
+}
