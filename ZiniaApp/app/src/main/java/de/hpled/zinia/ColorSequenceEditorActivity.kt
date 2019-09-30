e111178b846b15a2e6a5e75ee9f94bf27566d2f6
fwiecz <55588979+fwiecz@users.noreply.github.com>
2019-09-30 16:59:00 +0200

    Implemented PreviewControllerFragment

--- ZiniaApp/app/src/main/java/de/hpled/zinia/ColorSequenceEditorActivity.kt
+++ ZiniaApp/app/src/main/java/de/hpled/zinia/ColorSequenceEditorActivity.kt
@@ -3,14 +3,32 @@ package de.hpled.zinia
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import de.hpled.zinia.entities.Device
+import de.hpled.zinia.fragments.OnPreviewControllerActionListener
+import de.hpled.zinia.fragments.PreviewControllerFragment
+import java.lang.IllegalStateException
 
-class ColorSequenceEditorActivity : AppCompatActivity() {
+class ColorSequenceEditorActivity : AppCompatActivity(), OnPreviewControllerActionListener {
+
+    private val previewController by lazy {
+        supportFragmentManager.findFragmentById(R.id.colorSequencePreview)
+                as PreviewControllerFragment
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_color_sequence_editor)
         supportActionBar?.setDefaultDisplayHomeAsUpEnabled(true)
     }
+
+    override fun onStart() {
+        super.onStart()
+        previewController.onPreviewControllerActionListener += this
+    }
+
+    override fun onPreviewPlay(device: Device) { }
+
+    override fun onPreviewStop(device: Device) { }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         when(item?.itemId) {
